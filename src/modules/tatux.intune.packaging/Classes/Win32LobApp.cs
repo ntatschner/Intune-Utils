206224101daf8adace6d7ef931b69d90bab20400
Nigel Tatschner <ntatschner@gmail.com>
2024-03-28 16:26:07 +0000

    Updated Win32LobApp class

--- src/modules/tatux.intune.packaging/Classes/Win32LobApp.cs
+++ src/modules/tatux.intune.packaging/Classes/Win32LobApp.cs
@@ -6,126 +6,86 @@ using System.Text.Json.Serialization;
 
 namespace Tatux.Intune.Packaging
 {
-	public enum MobileAppPublishingState
-	{
-		NotPublished,
-		Processing,
-		Published,
-	}
-	public enum WindowsArchitecture
-	{
-		None,
-		X86,
-		X64,
-		Arm,
-		Neutral,
-	}
-	public enum Win32LobAppRuleType
-	{
-		Requirement,
-		Detection,
-	}
-	public enum RunAsAccountType
-	{
-		User,
-		System,
-	}
-	public enum Win32LobAppRestartBehavior
-	{
-		Allow,
-		Forced,
-		NotAllowed,
-	}
-	public enum Win32LobAppReturnCodeType
-	{
-		Success,
-		Failure,
-	}
-	public enum Win32LobAppMsiPackageType
-	{
-		PerMachine,
-		PerUser,
-	}
-	public enum Win32LobAppRegistryRuleOperationType
-	{
-		Exists,
-		DoesNotExist,
-		String,
-		Integer,
-		Version,
-	}
-	public enum Win32LobAppRuleOperator
-	{
-		Equal,
-		NotEqual,
-		GreaterThan,
-		GreaterThanOrEqual,
-		LessThan,
-		LessThanOrEqual,
-		Contains,
-		NotContains,
-		BeginsWith,
-		EndsWith,
-		NotBeginsWith,
-		NotEndsWith,
-		NotContainsAll,
-		NotContainsAny,
-		NotContainsAllValues,
-		NotContainsAnyValues,
-		ContainsAll,
-		ContainsAny,
-		ContainsAllValues,
-	}
-    
 	// Main Class
 	public class Win32LobApp
-    {
-        public string OdataType { get; } = "#microsoft.graph.win32LobApp";
-        public string DisplayName { get; set; }
-        public string Description { get; set; } = string.Empty;
-        public string Publisher { get; set; }  
-        private string _largeIcon;
-        public string LargeIcon 
-        {
-            get 
-            { 
-                return _largeIcon; 
-            } 
-            set 
-            { 
-                if (System.IO.Path.IsPathFullyQualified(value))
-                {
-                    _largeIcon = value; 
-                }
-                else
-                {
-                    throw new ArgumentException("Value must be a fully qualified path.");
-                }
-            } 
-        }
-        public bool IsFeatured { get; set; }
-        public string PrivacyInformationUrl { get; set; }
-        public string InformationUrl { get; set; }
-        public string Owner { get; set; }
-        public string Developer { get; set; }
-        public string Notes { get; set; }
-        public MobileAppPublishingState PublishingState { get; set; }
-        public string CommittedContentVersion { get; set; }
-        public string FileName { get; set; }
-        public long Size { get; set; }
-        public string InstallCommandLine { get; set; }
-        public string UninstallCommandLine { get; set; }
-        public WindowsArchitecture ApplicableArchitectures { get; set; }
-        public long MinimumFreeDiskSpaceInMB { get; set; }
-        public long MinimumMemoryInMB { get; set; }
-        public long MinimumNumberOfProcessors { get; set; }
-        public long MinimumCpuSpeedInMHz { get; set; }
-        public List<Win32LobAppRule> Rules { get; set; }
-        public Win32LobAppInstallExperience InstallExperience { get; set; }
-        public List<Win32LobAppReturnCode> ReturnCodes { get; set; }
-        public Win32LobAppMsiInformation MsiInformation { get; set; }
-        public string SetupFilePath { get; set; }
-        public string MinimumSupportedWindowsRelease { get; set; }
+	{
+		public enum MobileAppPublishingState
+		{
+			NotPublished,
+			Processing,
+			Published,
+		}
+		public enum WindowsArchitecture
+		{
+			None,
+			X86,
+			X64,
+			Arm,
+			Neutral,
+		}
+		public string OdataType { get; } = "#microsoft.graph.win32LobApp";
+		public string DisplayName { get; set; }
+		public string Description { get; set; } = string.Empty;
+		public string Publisher { get; set; }
+		private string _largeIcon;
+		public string LargeIcon
+		{
+			get
+			{
+				return _largeIcon;
+			}
+			set
+			{
+				if (System.IO.Path.IsPathFullyQualified(value))
+				{
+					_largeIcon = value;
+				}
+				else
+				{
+					throw new ArgumentException("Value must be a fully qualified path.");
+				}
+			}
+		}
+		public bool IsFeatured { get; set; }
+		public string PrivacyInformationUrl { get; set; }
+		public string InformationUrl { get; set; }
+		public string Owner { get; set; }
+		public string Developer { get; set; }
+		public string Notes { get; set; }
+		public MobileAppPublishingState PublishingState { get; set; }
+		public string CommittedContentVersion { get; set; }
+		public string FileName { get; set; }
+		public long Size { get; set; }
+		public string InstallCommandLine { get; set; }
+		public string UninstallCommandLine { get; set; }
+		public WindowsArchitecture ApplicableArchitectures { get; set; }
+		public long MinimumFreeDiskSpaceInMB { get; set; }
+		public long MinimumMemoryInMB { get; set; }
+		public long MinimumNumberOfProcessors { get; set; }
+		public long MinimumCpuSpeedInMHz { get; set; }
+		public List<Win32LobAppRule> Rules { get; set; }
+		public Win32LobAppInstallExperience InstallExperience { get; set; }
+		public List<Win32LobAppReturnCode> ReturnCodes { get; set; }
+		public Win32LobAppMsiInformation MsiInformation { get; set; }
+		public string SetupFilePath { get; set; }
+		public string MinimumSupportedWindowsRelease { get; set; }
+
+		public struct MimeContent
+		{
+			public string OdataType { get; } = "microsoft.graph.mimeContent";
+			public string Type { get; set; } = "String";
+			public string Value { get; set; }
+
+			public MimeContent(string type, string value)
+			{
+				Type = type;
+				Value = value;
+			}
+			public string ToJson()
+			{
+				return JsonSerializer.Serialize(this);
+			}
+		}
 
 		private MimeContent GetIcon(string path)
 		{
@@ -137,7 +97,7 @@ namespace Tatux.Intune.Packaging
 					Convert.ToBase64String(System.IO.File.ReadAllBytes(path))
 				);
 				return _largeIconOutput;
-			} 
+			}
 			else
 			{
 				throw new ArgumentException("File not found.");
@@ -147,30 +107,49 @@ namespace Tatux.Intune.Packaging
 		{
 			return JsonSerializer.Serialize(this);
 		}
-    }
-	public struct MimeContent 
-	{
-		public string OdataType { get; } = "microsoft.graph.mimeContent";
-		public string Type { get; set; } = "String";
-		public string Value { get; set; }
-
-		public MimeContent(string type, string value)
-		{
-			Type = type;
-			Value = value;
-		}
-		public string ToJson()
-		{
-			return JsonSerializer.Serialize(this);
-		}
 	}
 	public class Win32LobAppRule
 	{
+		public enum Win32LobAppRuleType
+		{
+			Requirement,
+			Detection,
+		}
 		public string OdataType { get; set; }
 		public Win32LobAppRuleType RuleType { get; set; }
 	}
 	public class Win32LobAppRegistryRule : Win32LobAppRule
 	{
+		public enum Win32LobAppRuleOperator
+		{
+			Equal,
+			NotEqual,
+			GreaterThan,
+			GreaterThanOrEqual,
+			LessThan,
+			LessThanOrEqual,
+			Contains,
+			NotContains,
+			BeginsWith,
+			EndsWith,
+			NotBeginsWith,
+			NotEndsWith,
+			NotContainsAll,
+			NotContainsAny,
+			NotContainsAllValues,
+			NotContainsAnyValues,
+			ContainsAll,
+			ContainsAny,
+			ContainsAllValues,
+		}
+		public enum Win32LobAppRegistryRuleOperationType
+		{
+			Exists,
+			DoesNotExist,
+			String,
+			Integer,
+			Version,
+		}
 		public bool Check32BitOn64System { get; set; }
 		public string KeyPath { get; set; }
 		public string ValueName { get; set; }
@@ -180,18 +159,40 @@ namespace Tatux.Intune.Packaging
 	}
 	public class Win32LobAppInstallExperience
 	{
+		public enum RunAsAccountType
+		{
+			User,
+			System,
+		}
+		public enum Win32LobAppRestartBehavior
+		{
+			Allow,
+			Forced,
+			NotAllowed,
+		}
+
 		public string OdataType { get; set; }
 		public RunAsAccountType RunAsAccount { get; set; }
 		public Win32LobAppRestartBehavior DeviceRestartBehavior { get; set; }
 	}
 	public class Win32LobAppReturnCode
 	{
+		public enum Win32LobAppReturnCodeType
+		{
+			Success,
+			Failure,
+		}
 		public string OdataType { get; set; }
 		public int ReturnCode { get; set; }
 		public Win32LobAppReturnCodeType Type { get; set; }
 	}
 	public class Win32LobAppMsiInformation
 	{
+		public enum Win32LobAppMsiPackageType
+		{
+			PerMachine,
+			PerUser,
+		}
 		public string OdataType { get; set; }
 		public string ProductCode { get; set; }
 		public string ProductVersion { get; set; }
@@ -201,5 +202,5 @@ namespace Tatux.Intune.Packaging
 		public string ProductName { get; set; }
 		public string Publisher { get; set; }
 	}
-	
+
 }
