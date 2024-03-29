25a3dcadc5807943acd9bc7490e6e2fab9c412f0
Nigel Tatschner <ntatschner@gmail.com>
2024-03-29 20:22:27 +0000

    Moving from c# class to ps function for json creation

---
+++ src/modules/tatux.intune.packaging/Private/New-Win32JSON.ps1
@@ -0,0 +1,48 @@
+# Takes in a JSON file and generates a Win32LOBApp JSON Object for List, Get, Create, Delete and Update operations.
+function New-Win32JSON {
+    [CmdletBinding()]
+    param (
+        [Parameter(ParameterSetName = 'File')]
+        [ValidateScript({
+                if (Test-Path -Path $_) {
+                    if ($null -ne [System.Text.Json.JsonDocument]::Parse($(Get-Content -Path $_ -Raw))) { 
+                        $true 
+                    }
+                    else { 
+                        throw "$Path is not a valid JSON file."
+                    }
+                }
+                else { 
+                    throw "File does not exist."
+                }
+            })]
+        [string]$Path,
+        [Parameter(ParameterSetName = 'Content')]
+        [ValidateScript({
+                $JSONTest = [System.Text.Json.JsonDocument]::Parse($_)
+                if ($null -ne $JSONTest) { 
+                    $true 
+                }
+                else { 
+                    throw "Input string is not a valid JSON."
+                }
+            })]
+        [string]$JSONContent,
+        [Parameter(ParameterSetName = 'Content')]
+        [Parameter(ParameterSetName = 'File')]
+        [ValidateSet('List', 'Get', 'Create', 'Delete', 'Update')]
+        [string]$ReturnType
+    )
+    
+    begin {
+        # 
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+    }
+}
