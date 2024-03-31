538e6517c4b26d0df872da456bbbc43e6367aa82
Nigel Tatschner <ntatschner@gmail.com>
2024-03-31 18:41:54 +0100

    Added more to the new-intunejson and added a new new-intunerule function.

--- src/modules/tatux.intune.packaging/Private/New-Win32Rule.ps1
+++ src/modules/tatux.intune.packaging/Private/New-Win32Rule.ps1
@@ -2,6 +2,9 @@
 function New-Win32Rule {
     [CmdletBinding()]
     param (
+        [Parameter(HelpMessage = 'The type of rule to create.')]
+        [Parameter(ParameterSetName = 'detection', HelpMessage = 'A detection rule type.')]
+        [Parameter(ParameterSetName = 'requirement', HelpMessage = 'A requirements rule type.')]
         [Parameter(Mandatory = $true)]
         [ValidateSet('requirement', 'detection')]
         [string]$RuleParentType,
@@ -11,33 +14,92 @@ function New-Win32Rule {
         [Parameter(ParameterSetName = 'file')]
         [Parameter(ParameterSetName = 'registry')]
         [Parameter(ParameterSetName = 'script')]
+        [Parameter(ParameterSetName = 'detection')]
+        [Parameter(ParameterSetName = 'requirement')]
         [string]$RuleType,
 
         [Parameter(ParameterSetName = 'file', Mandatory, HelpMessage = 'The path to the file or folder.')]
         [Parameter(ParameterSetName = 'registry', Mandatory, HelpMessage = 'The registry key path.')]
         [ValidateScript({
-            if (Test-Path -Path $_ -IsValid) {
-                $true
-            }
-            else {
-                throw "Path dosn't seem to be valid."
-            }
-        })]
+                if (Test-Path -Path $_ -IsValid) {
+                    $true
+                }
+                else {
+                    throw "Path dosn't seem to be valid."
+                }
+            })]
         [string]$Path,
 
-        [Parameter(Mandatory = $true)]
-        [string]$RulePayload
+        [Parameter(ParameterSetName = 'file', Mandatory, HelpMessage = 'The file or folder name.')]
+        [Parameter(ParameterSetName = 'detection')]
+        [Parameter(ParameterSetName = 'requirement')]
+        [string]$FileOrFolderName,
+
+        [Parameter(ParameterSetName = 'file', Mandatory, HelpMessage = 'Expand 32bit variables on a 64bit system?')]
+        [Parameter(ParameterSetName = 'registry', Mandatory, HelpMessage = 'Expand 32bit variables on a 64bit system?')]
+        [bool]$Check32BitOn64System = $true,
+
+        [Parameter(ParameterSetName = 'file', Mandatory, HelpMessage = 'The path or file operation type. If exists is selected then the operator and comparison value are not required.')]
+        [ValidateSet('notConfigured', 'exists', 'modifiedDate', 'createdDate', 'version', 'sizeInMB')]
+        [string]$FileOperationType = 'exists',
+
+        [Parameter(HelpMessage = 'The operator for comparison. Do not set for detection rules.')]
+        [Parameter(ParameterSetName = 'file', HelpMessage = 'The operator for comparison.')]
+        [Parameter(ParameterSetName = 'registry', HelpMessage = 'The operator for comparison.')]
+        [Parameter(ParameterSetName = 'script', HelpMessage = 'The operator for comparison. ')]
+        [ValidateSet('notConfigured', 'equal', 'notEqual', 'greaterThan', 'greaterThanOrEqual', 'lessThan', 'lessThanOrEqual')]
+        [string]$Operator = 'notConfigured',
+
+        [Parameter(ParameterSetName = 'file', HelpMessage = 'The value to compare against.')]
+        [Parameter(ParameterSetName = 'registry', HelpMessage = 'The value to compare against.')]
+        [string]$ComparisonValue,
+
+        [Parameter(ParameterSetName = 'registry', Mandatory, HelpMessage = 'The registry value name to detect.')]
+        [string]$ValueName,
+
+        [Parameter(ParameterSetName = 'registry', Mandatory, HelpMessage = 'The registry operation type. If exists is selected then the operator and comparison value are not required.')]
+        [ValidateSet('notConfigured', 'exists', 'doesNotExist', 'string', 'integer', 'version')]
+        [string]$RegistryOperationType = 'exists',
+        
+        [Parameter(ParameterSetName = 'script', Mandatory, HelpMessage = 'The script display name. Default will be file name.')]
+        [string]$DisplayName,
+
+        [Parameter(ParameterSetName = 'script', Mandatory, HelpMessage = 'The enforces a script signature check.')]
+        [bool]$EnforceSignatureCheck = $false,
+
+        [Parameter(ParameterSetName = 'script', Mandatory, HelpMessage = 'Run script as 32bit on 64bit systems.')]
+        [bool]$RunAs32Bit = $false,
+
+        [Parameter(ParameterSetName = 'script', Mandatory, HelpMessage = 'The account type to run the requirements script as.')]
+        [ValidateSet('system', 'user')]
+        [string]$runAsAccount = 'system',
+
+        [Parameter(ParameterSetName = 'script', Mandatory, HelpMessage = 'The script path.')]
+        [ValidateScript({
+                if (Test-Path -Path $_) {
+                    if ($(Get-Item -Path $_).Extension -ne '.ps1' ) {
+                        throw "Script file must be a PowerShell script."
+                    }
+                    else {
+                        $true
+                    }
+                }
+                else {
+                    throw "Path isnt valid."
+                }
+            })]
+        [string]$ScriptPath
     )
     process {
         $hashTable = @{
-            "@odata.type" = "#microsoft.graph.win32LobAppFileSystemRule"
-            "ruleType" = $RuleParentType
-            "path" = "String"
-            "fileOrFolderName" = "String"
+            "@odata.type"          = "#microsoft.graph.win32LobAppFileSystemRule"
+            "ruleType"             = $RuleParentType
+            "path"                 = "String"
+            "fileOrFolderName"     = "String"
             "check32BitOn64System" = $true
-            "operationType" = "String"
-            "operator" = "String"
-            "comparisonValue" = "String"
+            "operationType"        = "String"
+            "operator"             = "String"
+            "comparisonValue"      = "String"
         }
     }
 }
