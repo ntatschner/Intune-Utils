272522ee9676874484ee9c886dad661e73c8a7c6
Nigel Tatschner <ntatschner@gmail.com>
2024-04-06 14:55:42 +0100

    Update Public

---
+++ src/modules/tatux.intune.packaging/Public/Get-MSIProperties.ps1
@@ -0,0 +1,41 @@
+function Get-MSIProperties {
+    PARAM (
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "MSI Database Filename", ValueFromPipeline = $true)]
+        [Alias("Filename", "Path", "Database", "Msi")]
+        $msiDbName
+    )
+ 
+    # A quick check to see if the file exist
+    if (!(Test-Path $msiDbName)) {
+        throw "Could not find " + $msiDbName
+    }
+ 
+    # Create an empty hashtable to store properties in
+     
+    # Creating WI object and load MSI database
+    $WindowsInstaller = New-Object -ComObject WindowsInstaller.Installer
+    $WindowsInstallerDatabase = $WindowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $null, $WindowsInstaller, @(($msiDbName), 0))
+     
+    # Open the Property-view
+    $WindowsInstallerDatabaseView = $WindowsInstallerDatabase.GetType().InvokeMember("OpenView", "InvokeMethod", $null, $WindowsInstallerDatabase, "SELECT * FROM Property")
+    $WindowsInstallerDatabaseView.GetType().InvokeMember("Execute", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
+     
+    $Results = @{}
+	
+    # Loop thru the table
+    $WindowsInstallerDatabaseRow = $WindowsInstallerDatabaseView.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
+    while ($null -ne $WindowsInstallerDatabaseRow) {
+        # Add property and value to hash table
+        $name = $WindowsInstallerDatabaseView.GetType().InvokeMember("StringData", "GetProperty", $null, $WindowsInstallerDatabaseRow, 1)
+        $value = $WindowsInstallerDatabaseView.GetType().InvokeMember("StringData", "GetProperty", $null, $WindowsInstallerDatabaseRow, 2)
+        $Results."$name" = $value
+
+        # Fetch the next row
+        $WindowsInstallerDatabaseRow = $WindowsInstallerDatabaseView.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
+    }
+
+    $WindowsInstallerDatabaseView.GetType().InvokeMember("Close", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
+     
+    # Return the hash table
+    [PSCustomObject]$Results
+}
