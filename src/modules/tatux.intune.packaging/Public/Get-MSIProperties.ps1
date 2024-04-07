6e4c8c13d726426de80cacf3f01501339d432cfa
Nigel Tatschner <ntatschner@gmail.com>
2024-04-07 16:15:14 +0100

    Updating workflow for pester tests

--- src/modules/tatux.intune.packaging/Public/Get-MSIProperties.ps1
+++ src/modules/tatux.intune.packaging/Public/Get-MSIProperties.ps1
@@ -2,43 +2,44 @@ function Get-MSIProperties {
     param (
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, HelpMessage = "MSI Database Filename", ValueFromPipeline)]
         [ValidateScript({
-        if (!(Test-Path $_)) {
-            throw "Could not find " + $_
-            } else {
-               $true
-        }
-        })]
+                if (!(Test-Path $_)) {
+                    throw "Could not find " + $_
+                }
+                else {
+                    $true
+                }
+            })]
         [Alias("Filename", "Path", "Database", "Msi")]
         $msiDbName
     )
-   process {
-    # Create an empty hashtable to store properties in
+    process {
+        # Create an empty hashtable to store properties in
      
-    # Creating WI object and load MSI database
-    $WindowsInstaller = New-Object -ComObject WindowsInstaller.Installer
-    $WindowsInstallerDatabase = $WindowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $null, $WindowsInstaller, @(($msiDbName), 0))
+        # Creating WI object and load MSI database
+        $WindowsInstaller = New-Object -ComObject WindowsInstaller.Installer
+        $WindowsInstallerDatabase = $WindowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $null, $WindowsInstaller, @(($msiDbName), 0))
      
-    # Open the Property-view
-    $WindowsInstallerDatabaseView = $WindowsInstallerDatabase.GetType().InvokeMember("OpenView", "InvokeMethod", $null, $WindowsInstallerDatabase, "SELECT * FROM Property")
-    $WindowsInstallerDatabaseView.GetType().InvokeMember("Execute", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
+        # Open the Property-view
+        $WindowsInstallerDatabaseView = $WindowsInstallerDatabase.GetType().InvokeMember("OpenView", "InvokeMethod", $null, $WindowsInstallerDatabase, "SELECT * FROM Property")
+        $WindowsInstallerDatabaseView.GetType().InvokeMember("Execute", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
      
-    $Results = @{}
+        $Results = @{}
 	
-    # Loop thru the table
-    $WindowsInstallerDatabaseRow = $WindowsInstallerDatabaseView.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
-    while ($null -ne $WindowsInstallerDatabaseRow) {
-        # Add property and value to hash table
-        $name = $WindowsInstallerDatabaseView.GetType().InvokeMember("StringData", "GetProperty", $null, $WindowsInstallerDatabaseRow, 1)
-        $value = $WindowsInstallerDatabaseView.GetType().InvokeMember("StringData", "GetProperty", $null, $WindowsInstallerDatabaseRow, 2)
-        $Results."$name" = $value
-
-        # Fetch the next row
+        # Loop thru the table
         $WindowsInstallerDatabaseRow = $WindowsInstallerDatabaseView.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
-    }
+        while ($null -ne $WindowsInstallerDatabaseRow) {
+            # Add property and value to hash table
+            $name = $WindowsInstallerDatabaseView.GetType().InvokeMember("StringData", "GetProperty", $null, $WindowsInstallerDatabaseRow, 1)
+            $value = $WindowsInstallerDatabaseView.GetType().InvokeMember("StringData", "GetProperty", $null, $WindowsInstallerDatabaseRow, 2)
+            $Results."$name" = $value
 
-    $WindowsInstallerDatabaseView.GetType().InvokeMember("Close", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
+            # Fetch the next row
+            $WindowsInstallerDatabaseRow = $WindowsInstallerDatabaseView.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
+        }
+
+        $WindowsInstallerDatabaseView.GetType().InvokeMember("Close", "InvokeMethod", $null, $WindowsInstallerDatabaseView, $null)
      
-    # Return the hash table
-    [PSCustomObject]$Results
+        # Return the hash table
+        [PSCustomObject]$Results
     }
 }
