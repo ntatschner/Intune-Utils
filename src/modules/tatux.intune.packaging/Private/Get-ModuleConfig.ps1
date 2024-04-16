9dec485e571c12f0dbb46efa8c48a55c35f198cd
Nigel Tatschner <ntatschner@gmail.com>
2024-04-16 17:31:59 +0100

    fixes

--- src/modules/tatux.intune.packaging/Private/Get-ModuleConfig.ps1
+++ src/modules/tatux.intune.packaging/Private/Get-ModuleConfig.ps1
@@ -5,7 +5,9 @@ function Get-ModuleConfig {
     $ConfigDefaults = Join-Path -Path $ConfigPath -ChildPath "\Config\Module.Defaults.json"
     # Test to see if module config JSON exists and create it if it doesn't
     if (-not (Test-Path -Path $ConfigPath)) {
-        $DefaultConfig = Get-Content -Path $ConfigDefaults | ConvertFrom-Json -AsHashtable
+        $DefaultConfig = Get-Content -Path $ConfigDefaults | ConvertFrom-Json
+        $HashTable = @{}
+        $DefaultConfig.PSObject.Properties | ForEach-Object { $HashTable[$_.Name] = $_.Value }
         Set-ModuleConfig @DefaultConfig
         Get-ModuleConfig
     } else {
--- src/modules/tatux.intune.packaging/tatux.intune.packaging.psd1
+++ src/modules/tatux.intune.packaging/tatux.intune.packaging.psd1
@@ -12,7 +12,7 @@
     # RootModule = ''
 
     # Version number of this module.
-    ModuleVersion        = '0.9.0.5'
+    ModuleVersion        = '0.9.0.6'
 
     # Supported PSEditions
     CompatiblePSEditions = @('Desktop')
